bea2fd02161422084b3c4c2a59b8ae99443ded05
Ubuntu <ubuntu@ip-172-31-86-174.ec2.internal>
2022-01-17 23:43:53 +0000

    updated the copy string program.

--- copy_string.py
+++ copy_string.py
@@ -2,38 +2,40 @@
 
 # Created by: Ketia Gaelle Kaze
 # Created on: Jan. 10, 2022
-# This program asks the user to enter an integer,n, and a string 
+# This program asks the user to enter an integer,n, and a string
 # then it uses a loop to return a larger string that is n copies
 # of the original string.
 
 def copy_string():
-  # initialize the loop counter and the   
+  # initialize the loop counter and the string copy
   loop_counter = 0
-  string_copy = 0
-  
+  string_copy = ""
+
   # get user input as string
-  user_number_string = input("Enter a number: ")
-  user_string = input("Enter a word: ")
+  user_string = input("\033[1;36;38mEnter a string: ")
+  user_number_string = input("\033[1;36;38mEnter a number: ")
   print("")
-  
+
   # checks to catch errors
   try:
-      
       # covert user number string to an integer
       user_number_int = int(user_number_string)
       # checks to see if user number string is a whole number
-      if (user_number_int>=0):
+      if (user_number_int > 0):
           # calculate the copies of the user string using the user number string
-          for loop_counter in range(user_number_int + 1):
-              string_copy = loop_counter*user_string 
-          print("original string is : {} ".format(user_string))
-          print("New string is : {} ".format(string_copy))
+          for loop_counter in range(user_number_int):
+              string_copy = string_copy + user_string 
+          print("\033[1;33;38mOriginal string is : {} ".format(user_string))
+          print("\033[1;33;38mNew string is : {} ".format(string_copy))
       else:
-          print("{} is not a positive number".format(user_number_string))
+          print("\033[1;32;38m{} is not a valid input. Please "
+                  "enter a positive number.".format(user_number_string))
   except Exception:
-      print("{} is not a number.".format(user_number_string))
+      print("\033[1;31;38m{} is not a number. Please try again."
+                   .format(user_number_string))
   finally:
-      print("Thanks for playing")
+    print ("")
+    print("\033[1;35;38mThanks for playing.")
 
 def main():
     # This function calls other functions
